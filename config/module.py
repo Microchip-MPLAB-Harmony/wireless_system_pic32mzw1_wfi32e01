3f69653487f32f9ae7d6712b566318e6f75884ac
Himanshu Seth <himanshu.seth@microchip.com>
2021-07-13 14:23:38 +0530

    WSR2-540: Moved the Mqtt and Net Service from under 'Harmony Networking' to 'Wireless System Services'

--- config/module.py
+++ config/module.py
@@ -10,11 +10,11 @@ def loadModule():
     sysWifiProvPic32mzw1Component = Module.CreateComponent('sysWifiProvPic32mzw1', 'WIFI PROVISIONING SERVICE', '/Wireless/System Services/', 'system/wifiprov/config/sys_wifiprov.py')
     #sysWifiProvPic32mzw1Component.setDisplayType("WiFi Provision System Service")
 	########################## Harmony Network Net Service #################################    
-    netComponent = Module.CreateComponent("sysNetPic32mzw1", "Net Service", "/Harmony/Harmony Networking","system/net/config/sys_net.py")
+    netComponent = Module.CreateComponent("sysNetPic32mzw1", "Net Service", '/Wireless/System Services/',"system/net/config/sys_net.py")
     #netComponent.addCapability("libNet","net",True)    
     #netComponent.addDependency("Net_Crypto_Dependency", "TLS Provider", None, False, False)
 
     ############################### Third Party wolfSSL Module #####################################
     pahomqttComponent = Module.CreateComponent('lib_pahomqtt', 'Paho MQTT Library', '/Third Party Libraries/PahoMqtt/', 'config/pahomqtt.py')
 
-    mqttComponent = Module.CreateComponent('sysMqttPic32mzw1', 'MQTT Service', '/Harmony/Harmony Networking','system/mqtt/config/sys_mqtt.py')
+    mqttComponent = Module.CreateComponent('sysMqttPic32mzw1', 'MQTT Service', '/Wireless/System Services/', 'system/mqtt/config/sys_mqtt.py')
